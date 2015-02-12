55bbe643a45bcd75fddc84c2727e70e97ce65afd
jozadaquebatista <jozadaquebatista@gmail.com>
2015-02-12 17:25:35 -0200

    Create vetorTeste.c

---
+++ vetorTeste.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+        
+    int *vetor;
+    
+    vetor = malloc(sizeof(int));
+    
+    *(vetor+0) = 61;
+    *(vetor+1) = 62;
+    *(vetor+2) = 63;
+    *(vetor+3) = 64;
+    *(vetor+4) = 65;
+    *(vetor+5) = 66;
+    *(vetor+6) = 67;
+    *(vetor+7) = 68;
+
+    for(int i = 0; i < *vetor; ++i && ++vetor){
+        fprintf(stdout, "%d\n", *(vetor));
+    }
+    
+    fprintf(stdout, "Fora do loop: %d\n", *(vetor=vetor-8));
+    
+    return(0);
+}
